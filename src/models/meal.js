184f0e0574a2723b9b9572d7ce32fe315e839e32
Patrick Njoli <patimjoli@gmail.com>
2020-10-16 13:45:24 +0100

    updated the meal schema

---
+++ src/models/meal.js
@@ -0,0 +1,19 @@
+const mongosse =  require('mongoose');
+const Schema = mongosse.Schema;
+
+const mealSchema = new Schema({
+    _id: mongosse.Types.ObjectId,
+    name:  {
+        type: String,
+        // required: true
+    },
+    quantity:   {
+        type: String,
+        //required: true
+    },
+    price: {
+        type: String,
+        //required: true
+    }
+})
+module.exports = mongosse.model('Meal', mealSchema);
