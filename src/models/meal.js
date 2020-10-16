ce0d5eb0365fb7cd958b80b43ef360b2c7a4e3de
Patrick Njoli <patimjoli@gmail.com>
2020-10-16 14:28:02 +0100

    refrence the order Schema to meal

--- src/models/meal.js
+++ src/models/meal.js
@@ -4,16 +4,12 @@ const Schema = mongosse.Schema;
 const mealSchema = new Schema({
     _id: mongosse.Schema.Types.ObjectId,
     name:  {
-        type: mongosse.Schema.Types.ObjectId
-        // required: true
-    },
-    quantity:   {
-        type: String,
-        //required: true
+        type: mongosse.Schema.Types.ObjectId,
+        required: true
     },
     price: {
         type: String,
-        //required: true
+        required: true
     }
 })
 module.exports = mongosse.model('Meal', mealSchema);
---
+++ src/models/orders.js
@@ -0,0 +1,14 @@
+const mongosse =  require('mongoose');
+const Schema = mongosse.Schema;
+
+const mealSchema = new Schema({
+    _id: mongosse.Schema.Types.ObjectId,
+    name:  {
+        type: mongosse.Schema.Types.ObjectId, ref: 'Meal'
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    }
+})
+module.exports = mongosse.model('Order', orderSchema);
